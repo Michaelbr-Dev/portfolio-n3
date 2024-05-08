f8861a2db34fa0e3474fc94bc8bb77e1d72e4d21
Michael Briquet <contact@michaelbr-dev.fr>
2024-05-08 12:22:17 +0200

    🎉 Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    "@nuxt/eslint",
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error is not undefined
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    "@nuxt/imagee",
+    "@nuxtjs/google-fonts",
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  eslint: {
+    // options here
+  },
+});
