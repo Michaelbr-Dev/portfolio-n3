f76d7f818cdcc6b40f282cfda45f7827616d223a
Michael Briquet <contact@michaelbr-dev.fr>
2024-05-09 19:03:13 +0200

    ✨ Add sidebar menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,66 @@
 <template>
-  <v-card>
-    <v-layout>
-      <v-navigation-drawer class="bg-deep-purple" permanent>
-        <v-list-item
-          lines="two"
-          prepend-avatar="/assets/images/profile_bw.png"
-          title="Michael Briquet"
-          subtitle="Développeur Web Junior"
-        ></v-list-item>
-        <v-divider></v-divider>
-        <v-list-item prepend-icon="mdi-home" link title="Accueil"></v-list-item>
-        <v-list-item prepend-icon="mdi-folder" link title="Portfolio"></v-list-item>
-        <v-list-item prepend-icon="mdi-email" link title="Contact"></v-list-item>
-        <v-divider></v-divider>
-        <div>
-          <p>
-            Bonjour, je m'appel Michaël et je suis un développeur web junior de sites et
-            d'applications évolutives. Bienvenue sur mon portfolio !
-          </p>
+  <v-layout>
+    <v-navigation-drawer class="bg-deep-purple" permanent>
+      <div class="text-center my-3">
+        <p class="text-h5 font-weight-bold">Michael Briquet</p>
+        <h1 class="text-subtitle-1">Développeur Web Front</h1>
+      </div>
+      <v-img class="mx-auto mb-3" :width="160" src="/assets/images/profile_bw.png" />
+      <v-divider />
+      <v-list class="mx-auto my-2">
+        <v-list-item prepend-icon="mdi-home" link to="/" title="Accueil" />
+
+        <v-list-item class="mx-auto" prepend-icon="mdi-folder" link title="Portfolio" />
+
+        <v-list-item class="mx-auto" prepend-icon="mdi-email" link title="Contact" />
+      </v-list>
+      <v-divider />
+      <div class="my-3">
+        <p class="px-3 mb-3 text-body-2">
+          Bonjour, je m'appel Michaël et je suis un développeur web frontend de sites et
+          d'applications évolutives. <br />Bienvenue sur mon portfolio !
+        </p>
+        <v-divider />
+        <div class="mt-3 d-flex justify-center">
+          <v-hover>
+            <template #default="{ isHovering, props }">
+              <NuxtLink
+                title="linkedin"
+                v-bind="props"
+                class="mx-1 social-link"
+                :class="{ 'text-grey-lighten-1': isHovering, 'text-white': !isHovering }"
+                to="https://www.linkedin.com/in/michael-briquet/"
+                target="_blank"
+              >
+                <v-icon icon="mdi-linkedin" />
+              </NuxtLink>
+            </template>
+          </v-hover>
+          <v-hover>
+            <template #default="{ isHovering, props }">
+              <NuxtLink
+                title="github"
+                v-bind="props"
+                class="mx-1 social-link"
+                :class="{ 'text-grey-lighten-1': isHovering, 'text-white': !isHovering }"
+                to="https://github.com/Michaelbr-Dev"
+                target="_blank"
+              >
+                <v-icon icon="mdi-github" />
+              </NuxtLink>
+            </template>
+          </v-hover>
         </div>
-      </v-navigation-drawer>
-      <v-main>
-        <slot />
-      </v-main>
-    </v-layout>
-  </v-card>
+      </div>
+    </v-navigation-drawer>
+    <v-main>
+      <slot />
+    </v-main>
+  </v-layout>
 </template>
+
+<style scoped>
+.social-link {
+  transition: all 0.3s ease-in-out;
+}
+</style>
